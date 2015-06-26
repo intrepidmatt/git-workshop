80b4e2d77f46630e62c6b71ac64e06325b53a8aa
Matt Bridges <matt@intrepid.io>
2015-06-26 15:20:52 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+console.log("Matt's Favorite Things:");
+var mattsFavs = require('./matts_favs.js');
+mattsFavs.beer();
+mattsFavs.band();
+mattsFavs.show();
+mattsFavs.movie();
---
+++ matts_favs.js
@@ -0,0 +1,14 @@
+module.exports = {
+  beer: function() {
+    console.log("* Budweiser");
+  },
+  band: function() {
+    console.log("* Madeon");
+  },
+  show: function() {
+    console.log("* Game of Thrones");
+  },
+  movie: function() {
+    console.log("* Back to the Future (I)");
+  }
+}
